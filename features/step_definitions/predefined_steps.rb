26c13ac1c7d8a4ff684692eac1ba2eea28e90ecd
vskryabin <vs8199@gmail.com>
2018-09-16 15:48:48 -0700

    Fixes

--- features/step_definitions/predefined_steps.rb
+++ features/step_definitions/predefined_steps.rb
@@ -4,7 +4,7 @@ require_relative "../support/hooks"
 Then(/^element having (.+) "([^\"]*)" should\s*((?:not)?)\s+have attribute "(.*?)" with partial value "(.*?)"$/) do |locator_type, locator_value, reverse_match, attribute_name, attribute_expected_value|
   is_valid_type = %w(id class css name xpath).include? locator_type
   unless is_valid_type
-    raise "Invalid locator type - " + locator_type
+    raise "Unsupported locator type - " + locator_type
   end
   attribute_actual_value = $driver.find_element(:"#{locator_type}", locator_value).attribute(attribute_name)
   if reverse_match.empty?
